2c1a568c8f9d41d56e64c6a61dc420016710bf17
PERSONAL <PERSONAL@DESKTOP-4GCPVC0>
2020-11-16 18:41:55 -0500

    Se sube el proyecto sin arreglos del FrontEndAgregar .gitignore y .gitattributes.

---
+++ Restaurant/Controllers/CLIENTEsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant.Models;
+
+namespace Restaurant.Controllers
+{
+    public class CLIENTEsController : Controller
+    {
+        private RestauranteEntities db = new RestauranteEntities();
+
+        // GET: CLIENTEs
+        public ActionResult Index()
+        {
+            return View(db.CLIENTE.ToList());
+        }
+
+        // GET: CLIENTEs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CLIENTE cLIENTE = db.CLIENTE.Find(id);
+            if (cLIENTE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cLIENTE);
+        }
+
+        // GET: CLIENTEs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CLIENTEs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_CLIENTE,NOMBRE1,NOMBRE2,APELLIDO1,APELLIDO2,DIRECCION")] CLIENTE cLIENTE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CLIENTE.Add(cLIENTE);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cLIENTE);
+        }
+
+        // GET: CLIENTEs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CLIENTE cLIENTE = db.CLIENTE.Find(id);
+            if (cLIENTE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cLIENTE);
+        }
+
+        // POST: CLIENTEs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_CLIENTE,NOMBRE1,NOMBRE2,APELLIDO1,APELLIDO2,DIRECCION")] CLIENTE cLIENTE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cLIENTE).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cLIENTE);
+        }
+
+        // GET: CLIENTEs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CLIENTE cLIENTE = db.CLIENTE.Find(id);
+            if (cLIENTE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cLIENTE);
+        }
+
+        // POST: CLIENTEs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CLIENTE cLIENTE = db.CLIENTE.Find(id);
+            db.CLIENTE.Remove(cLIENTE);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Restaurant/Controllers/MENUsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant.Models;
+
+namespace Restaurant.Controllers
+{
+    public class MENUsController : Controller
+    {
+        private RestauranteEntities db = new RestauranteEntities();
+
+        // GET: MENUs
+        public ActionResult Index()
+        {
+            var mENU = db.MENU.Include(m => m.CLIENTE);
+            return View(mENU.ToList());
+        }
+
+        // GET: MENUs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MENU mENU = db.MENU.Find(id);
+            if (mENU == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mENU);
+        }
+
+        // GET: MENUs/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_CLIENTE = new SelectList(db.CLIENTE, "ID_CLIENTE", "NOMBRE1");
+            return View();
+        }
+
+        // POST: MENUs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_MENU,ID_CLIENTE,NOMBRE,CATEGORIA,PRECIO")] MENU mENU)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MENU.Add(mENU);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_CLIENTE = new SelectList(db.CLIENTE, "ID_CLIENTE", "NOMBRE1", mENU.ID_CLIENTE);
+            return View(mENU);
+        }
+
+        // GET: MENUs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MENU mENU = db.MENU.Find(id);
+            if (mENU == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_CLIENTE = new SelectList(db.CLIENTE, "ID_CLIENTE", "NOMBRE1", mENU.ID_CLIENTE);
+            return View(mENU);
+        }
+
+        // POST: MENUs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_MENU,ID_CLIENTE,NOMBRE,CATEGORIA,PRECIO")] MENU mENU)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mENU).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_CLIENTE = new SelectList(db.CLIENTE, "ID_CLIENTE", "NOMBRE1", mENU.ID_CLIENTE);
+            return View(mENU);
+        }
+
+        // GET: MENUs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MENU mENU = db.MENU.Find(id);
+            if (mENU == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mENU);
+        }
+
+        // POST: MENUs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MENU mENU = db.MENU.Find(id);
+            db.MENU.Remove(mENU);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
